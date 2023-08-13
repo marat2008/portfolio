803690fd4dafcb19bb6ac5f49bba54c9fddb4da9
marat2008 <131581194+marat2008@users.noreply.github.com>
2023-08-13 11:05:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ def index():
 @app.route('/', methods=['POST'])
 def process_form():
     button_python = request.form.get('button_python')
-    return render_template('index.html', button_python=button_python)
+    button_python = request.form.get('button_discord')
+    return render_template('index.html', button_python=button_python, button_discord=button_python)
 
 
 if __name__ == "__main__":
